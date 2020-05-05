309462d3ac79dcfaff479f1f15dd1f4089a7589e
agatino.work@gmail.com <agatino.work@gmail.com>
2020-05-05 15:54:47 +0200

    Add project files.

---
+++ TestFormValidation/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestFormValidation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate bool AllFieldsValidatorDelegate();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public AllFieldsValidatorDelegate AllFieldsValidator
+        {
+            get { return (AllFieldsValidatorDelegate)GetValue(AllFieldsValidatorProperty); }
+            set { SetValue(AllFieldsValidatorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AllFieldsValidator.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AllFieldsValidatorProperty =
+            DependencyProperty.Register("AllFieldsValidator", typeof(AllFieldsValidatorDelegate), typeof(MainWindow), 
+                new PropertyMetadata(null, AllFieldsValidator_PropertyChanged));
+
+        private static void AllFieldsValidator_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d == null)
+                return;
+            return
+        }
+
+        private bool IsValid(DependencyObject obj)
+        {
+            // The dependency object is valid if it has no errors and all
+            // of its children (that are dependency objects) are error-free.
+            return !Validation.GetHasError(obj) &&
+            LogicalTreeHelper.GetChildren(obj)
+            .OfType<DependencyObject>()
+            .All(IsValid);
+        }
+    }
+}
---
+++ TestFormValidation/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestFormValidation
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private bool _canSave; public bool CanSave { get => _canSave; set { Set(() => CanSave, ref _canSave, value); }}
+        private double _dvalue; public double Dvalue { get => _dvalue; set { Set(() => Dvalue, ref _dvalue, value); }}
+        private string _svalue; public string Svalue { get => _svalue; set { Set(() => Svalue, ref _svalue, value); }}
+
+        private RelayCommand _saveCmd;
+        public RelayCommand SaveCmd => _saveCmd ?? (_saveCmd = new RelayCommand(
+            () => saveCmdExecuted(),
+            () => saveCmdCanExecute()
+            ));
+
+        private void saveCmdExecuted()
+        {
+            MessageBox.Show("Saved!");
+        }
+
+        private bool  saveCmdCanExecute()
+        {
+            BindingOperations.GetSourceUpdatingBindings()
+            return true;
+        }
+    }
+}
